6bfd19cd875caeae41f6d10d4d3ab4de0a5843ed
Kanguijin <wheretogobro@naver.com>
2023-08-12 18:21:41 +0900

    AJAX로 삭제기능 구현, Bootstrap 디자인 넣기, HTML 조립식 개발

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.use(express.urlencoded({extended: true}));
 const MongoClient = require('mongodb').MongoClient;
 app.set('view engine', 'ejs');
 
+app.use('/public', express.static('public'));
+
+
+
 var db;
 MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.wtvmcud.mongodb.net/?retryWrites=true&w=majority', function(에러, client){
     // 연결되면 할 일
@@ -34,10 +38,10 @@ app.get('/beauty', function(요청, 응답){
 });
 
 app.get('/', function(요청, 응답){
-    응답.sendFile(__dirname + '/index.html');
+    응답.render('index.ejs')
 });
 app.get('/write', function(요청, 응답){
-    응답.sendFile(__dirname + '/write.html');
+    응답.render('write.ejs');
 });
 
 // 어떤 사람이 /add 경로로 POST요청을 하면...
@@ -73,6 +77,22 @@ app.get('/list', function(요청, 응답){
 
 });
 
+app.delete('/delete', function(요청, 응답){
+    console.log(요청.body);
+    요청.body._id = parseInt(요청.body._id);
+    // 요청.body에 담겨온 게시물번호를 가진 글을 db에서 찾아서 삭제해주세요
+    db.collection('post').deleteOne(요청.body, function(에러,결과){
+        console.log('삭제완료');
+        응답.status(200).send({ message : '성공했습니다'});
+    });
+})
+
+app.get('/detail/:id', function(요청, 응답){
+    db.collection('post').findOne({_id : parseInt(요청.params.id)}, function(에러, 결과){
+        console.log(결과);
+        응답.render('detail.ejs', { data : 결과 });
+    });
+})
 
 
 
